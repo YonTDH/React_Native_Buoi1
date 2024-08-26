94bc696a78981c9ff0295f87d0be5e2f37ad1487
YonTDH <tdh1503@gmail.com>
2024-08-26 15:50:39 +0700

    Xong Coding Challenge #2

--- Buoi2/21044471_TranDangHieu_Tuan2.js
+++ Buoi2/21044471_TranDangHieu_Tuan2.js
@@ -46,6 +46,7 @@ let koalasScore1 = 65;
 let koalasScore2 = 54;
 let koalasScore3 = 49;
 
+console.log("Coding Challenge #1:");
 let avgDolphins1 = calcAverage(dolphinsScore1, dolphinsScore2, dolphinsScore3);
 let avgKoalas1 = calcAverage(koalasScore1, koalasScore2, koalasScore3);
 
@@ -65,3 +66,59 @@ let avgKoalas2 = calcAverage(koalasScore4, koalasScore5, koalasScore6);
 
 console.log("Test data 2:");
 checkWinner(avgDolphins2, avgKoalas2);
+
+
+/*
+Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array) 
+*/ 
+
+const calcTip = (bill) => {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15;
+    } else {
+        return bill * 0.2;
+    }
+};
+
+const bills = [125, 555, 44];
+const tips = bills.map((bill) => calcTip(bill));
+const total = bills.map((bill, index) => bill + tips[index]);
+
+console.log("Coding Challenge #2:");
+console.log("Tips:", tips);
+console.log("Total:", total);
+
+/**
+ * Coding Challenge #3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall.
+ */
